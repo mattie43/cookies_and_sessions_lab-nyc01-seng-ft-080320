e69f0374b0acc9f4825d56e66b8ba547838447e3
Mattie43 <ericksenm93@ymail.com>
2020-09-02 14:36:09 -0400

    fixed add back to original

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,9 +4,7 @@ class ProductsController < ApplicationController
         # byebug
     end
     def add
-        cart = session[:cart] || []
         cart << params[:item]
-        session[:cart] = cart
         redirect_to products_path
     end
 end
